6f41ecd4fdf4c916bf8920f07b97bfa2366efb5b
dengerboi <maharnab.p@gmail.com>
2021-04-05 11:31:29 +0530

    Add project files.

---
+++ SecurityInMVC/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecurityInMVC.Models;
+using SecurityInMVC.Controllers;
+using System.Web.Security;//for web authentication
+
+namespace SecurityInMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        LTIMVCEntities db = new LTIMVCEntities();
+        // GET: Default
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index() //no auth required
+        {
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Register() //no auth required
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous] // skip authorization, begane log
+        public ActionResult Register(Employee emp)
+        {
+            db.Employees.Add(emp);
+            var res = db.SaveChanges();
+            if (res > 0)
+                ModelState.AddModelError("","Employee Registered");
+            return View();
+        }
+        [HttpGet]
+        [Authorize] //For Authentication lool
+        public ActionResult SelectEmployees()
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+
+    }
+}
